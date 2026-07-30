75813b400b3a76d4e5cc9db77e33151f5263f463
汪承祥 <worklibo@foxmail.com>
2026-07-30 19:26:06 +0800

    完善

--- assets/variant-picker.js
+++ assets/variant-picker.js
@@ -8,7 +8,21 @@ class VariantPicker extends HTMLElement{
   }
 
   #onInputChange(event) {
-    console.log(event.target.dataset);
+    const selectedOptions = this.querySelectorAll("fieldset input:checked");
+    const optionIds = []
+    selectedOptions.forEach(option => {
+      optionIds.push(option.dataset["id"])
+    })
+
+    const {productUrl, sectionId} = this.dataset
+
+    fetch(`${productUrl}?sectionId=${sectionId}&option_values=${optionIds.join(',')}`)
+      .then(response => response.text())
+      .then(text => {
+        const newPage = new DOMParser().parseFromString(text, 'text/html');
+        document.getElementById(sectionId).innerHTML = newPage.getElementById(sectionId).innerHTML;
+        console.log("success");
+      })
   }
 
   #dispatch() {
