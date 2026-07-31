295bacbdb2ce1848e78c70b7dab67be3a3fd5721
汪承祥 <worklibo@foxmail.com>
2026-07-31 12:27:02 +0800

    完善

--- assets/quantity-input.js
+++ assets/quantity-input.js
@@ -1,11 +1,16 @@
 class QuantityInput extends HTMLElement {
   quantityInput = undefined;
+  minusButton = undefined;
+  plusButton = undefined;
   constructor() {
     super();
   }
 
   connectedCallback() {
     this.quantityInput = this.querySelector("input[type='number']");
+    this.minusButton = this.querySelector("button[name='minus']");
+    this.plusButton = this.querySelector("button[name='plus']");
+
     this.quantityInput.addEventListener("change", this.#onInputChange.bind(this))
     this.querySelectorAll("button").forEach(button => {
       button.addEventListener("click", this.#onButtonClick.bind(this));
@@ -22,10 +27,31 @@ class QuantityInput extends HTMLElement {
       console.log("-")
       this.quantityInput.stepDown()
     }
+
+    this.#validateFields()
   }
 
   #onInputChange(event) {
     console.log("input change");
+    this.#validateFields()
+  }
+
+  #validateFields() {
+    const quantity = this.quantityInput.value;
+    const max = this.quantityInput.getAttribute("max");
+    const min = this.quantityInput.getAttribute("min");
+
+    if(max && quantity >= max) {
+      this.plusButton.setAttribute("disabled", "disabled");
+    } else {
+      this.plusButton.removeAttribute("disabled");
+    }
+
+    if(min && quantity <= min) {
+      this.minusButton.setAttribute("disabled", "disabled");
+    } else {
+      this.minusButton.removeAttribute("disabled");
+    }
   }
 
   #updateQuantity() {
