06f69a75ebf958d482132a242123a67adcb6af35
汪承祥 <worklibo@foxmail.com>
2026-09-14 19:40:44 +0800

    重写gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,56 @@
-const gulp = require("gulp");
+const { src, dest, parallel, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass-embedded"));
-const uglify = require("gulp-uglify");
 const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
 const fs = require("fs");
 
-const SOURCE_DIR = "src";
-const DEST_DIR = "assets";
+const srcDir = "src";
+const destDir = "assets";
 
+/**
+ * 处理 scss 文件
+ */
 function styles() {
-  return gulp.src(`${SOURCE_DIR}/css/**/*.scss`)
-    .pipe(sass({ style: 'compressed' }, null).on("error", sass.logError))
-    // .pipe(uglify())
-    .pipe(gulp.dest(DEST_DIR));
+  return src(`${srcDir}/css/**/*.scss`)
+    // noinspection JSCheckFunctionSignatures
+    .pipe(sass({ style: "compressed" })
+      .on("error", sass.logError))
+    .pipe(dest(destDir));
 }
 
-function getBundleTasks() {
-  return fs.readdirSync(`${SOURCE_DIR}/js`, {withFileTypes: true})
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => {
-      const dirName = dirent.name
-      const glob = `${SOURCE_DIR}/js/${dirName}/**/*.js`;
-      const outputName = `${dirName}.js`;
+/**
+ * 为 src/js 下每个子目录生成一个打包任务
+ */
+function getScriptTasks() {
+  return fs
+    .readdirSync(`${srcDir}/js`, { withFileTypes: true })
+    .filter((child) => child.isDirectory())
+    .map((dir) => {
+      const dirName = dir.name;
+      const glob = `${srcDir}/js/${dirName}/**/*.js`;
+      const bundleName = `${dirName}.js`;
 
+      // 返回一个任务函数，而不是 stream
       return function bundle() {
-        return gulp.src(glob).pipe(concat(outputName))
+        return src(glob)
+          .pipe(concat(bundleName))
           .pipe(uglify())
-          .pipe(gulp.dest(DEST_DIR));
-      }
-    })
-    ;
+          .pipe(dest(destDir));
+      };
+    });
 }
 
-const scripts = gulp.parallel(...getBundleTasks());
+/**
+ * scripts 是一个组合任务，执行所有子目录打包
+ */
+const scripts = parallel(...getScriptTasks());
 
-function watch() {
-  gulp.watch(`${SOURCE_DIR}/css/**/*.scss`, styles);
-  gulp.watch(`${SOURCE_DIR}/js/**/*.js`, scripts);
-}
+exports.styles = styles;
+exports.scripts = scripts;
+exports.build = parallel(styles, scripts);
 
-const build = gulp.parallel(styles, scripts);
-
-exports.default = gulp.series(build, watch);
+exports.default = function () {
+  watch(`${srcDir}/css/**/*.scss`, styles);
+  // 监听 js 变化，重新跑所有打包任务
+  watch(`${srcDir}/js/**/*.js`, scripts);
+};
